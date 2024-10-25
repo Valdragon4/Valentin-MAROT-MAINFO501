1b6a11b54e131a7d769e3490dc9c86df89e01c98
valdragon4 <valentin.marot@etudiant.univ-reims.fr>
2024-10-25 13:11:56 +0200

    25/10/2024

--- TP3/Assets/Scripts/rope.cs
+++ TP3/Assets/Scripts/rope.cs
@@ -1,30 +1,30 @@
 using UnityEngine;
 using System.Collections;
 
-public class onload : MonoBehaviour {
+public class rope : MonoBehaviour {
 
 
 
     public GameObject cubeBottom;
     public GameObject cubeTop;
+    public LineRenderer lineRenderer;
 
 
     void Start ()
     {
-
-        LineRenderer lineRenderer = GetComponent<LineRenderer> ();
-
-        lineRenderer.SetPosition (0, cubeBottom.transform.localPosition);
-        lineRenderer.SetPosition (1, cubeTop.transform.localPosition);
-        lineRenderer.endWidth = 0.1f;
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.material.color = Color.black;
+        lineRenderer.material = new Material (Shader.Find ("Sprites/Default"));
+        lineRenderer.startColor = Color.black;
+        lineRenderer.endColor = Color.black;
+        lineRenderer.startWidth = 0.02f;
+        lineRenderer.endWidth = 0.02f;
+        lineRenderer.positionCount = 2;
 
     }
 
     void Update ()
-    {
-
+    {  
+        lineRenderer.SetPosition (0, cubeBottom.transform.position);
+        lineRenderer.SetPosition (1, cubeTop.transform.position);
     }
 
 }
