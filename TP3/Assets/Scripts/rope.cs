4a7d85a37c40202698b4fe8b3dd6ff46e0cdf2c5
valdragon4 <valentin.marot@etudiant.univ-reims.fr>
2024-10-25 13:20:50 +0200

    25/10/2024

--- TP3/Assets/Scripts/rope.cs
+++ TP3/Assets/Scripts/rope.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-
-<<<<<<< HEAD
 public class rope : MonoBehaviour {
 
 
@@ -25,29 +23,6 @@ public class rope : MonoBehaviour {
     {  
         lineRenderer.SetPosition (0, cubeBottom.transform.position);
         lineRenderer.SetPosition (1, cubeTop.transform.position);
-=======
-public class rope : MonoBehaviour
-{
-    public Transform moufle;
-    public Transform crochet;
-    public LineRenderer lineRenderer;
-    private Vector3[] bezierPoints = new Vector3[4];
-
-    void Start()
-    {
-        lineRenderer.startWidth = 0.01f;
-        lineRenderer.endWidth = 0.01f;
-        lineRenderer.positionCount = 2;
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.startColor = Color.black;
-        lineRenderer.endColor = Color.black;
-    }
-
-    void Update()
-    {
-        lineRenderer.SetPosition(0, moufle.position);
-        lineRenderer.SetPosition(1, crochet.position);
->>>>>>> 635171c0fe0b7007c81f88373a2620c9ac89cddf
     }
 
 }
